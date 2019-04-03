4732c7ba31fdf980ec8b31a8376e9671b4633dc7
jacksun <m18657761931@163.com>
2019-04-03 23:50:59 +0800

    git commit

---
+++ app/src/main/java/com/example/administrator/hocoll/AuTofillActivity.java
@@ -0,0 +1,13 @@
+package com.example.administrator.hocoll;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuTofillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_au_tofill);
+    }
+}
